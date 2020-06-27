b678b0f899b7bde6692bb9493d8af597993809f7
pamAdmin <>
2020-06-27 15:44:47 -0600

    {/src/main/java/com/myspace/dynamic_subprocess_rules_kjar/Product.java}

--- src/main/java/com/myspace/dynamic_subprocess_rules_kjar/Product.java
+++ src/main/java/com/myspace/dynamic_subprocess_rules_kjar/Product.java
@@ -6,12 +6,36 @@ package com.myspace.dynamic_subprocess_rules_kjar;
 
 public class Product implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Product() {
+	private java.lang.String name;
+	private java.lang.Integer price;
+
+	public Product() {
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.Integer getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(java.lang.Integer price) {
+		this.price = price;
+	}
+
+	public Product(java.lang.String name, java.lang.Integer price) {
+		this.name = name;
+		this.price = price;
+	}
+    
+    public String toString() {
+        return "Product Name: " + name + "; price: $" + price;
     }
-
-
-
-
 }
